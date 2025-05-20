db416164364ff6bebc2b645463f572246097c389
sametyabalak <samety@evam.com>
2025-05-20 19:46:49 +0300

    Create package structe and add gitignore

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "EvamiOSCommonKit",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "EvamiOSCommonKit",
+            targets: ["EvamiOSCommonKit"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EvamiOSCommonKit",
+            path: "Sources/EvamiOSCommonKit/EvamiOSCommonKit.xcframework"
+        )
+    ]
+)
